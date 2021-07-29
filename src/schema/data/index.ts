c061f4aacaa746debd85af427b3ff689ac127040
soundquiet <625941917@qq.com>
2021-07-29 16:47:42 +0800

    feat: update data.values in spec and update examples

--- src/schema/data/index.ts
+++ src/schema/data/index.ts
@@ -1,8 +1,9 @@
 import { DataRow } from './dataRow';
+import { dataGraph } from './dataGraph';
 
 export interface Data {
   type?: 'json-array' | 'json' | 'url'; // inline array or external url
-  values: DataRow[] | string | object; // array or url
+  values: DataRow[] | string | dataGraph; // array or url
   format?: 'csv' | 'json'; // csv / json for parsing
   config?: DataConfig; // config for parsing
 }
