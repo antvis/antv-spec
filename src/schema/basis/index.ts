f4548217f554077ba6fa4f76065d990f457a7171
soundquiet <625941917@qq.com>
2021-07-29 11:30:23 +0800

    feat: add property 'type' in Basis to indicate visualization type

--- src/schema/basis/index.ts
+++ src/schema/basis/index.ts
@@ -1,5 +1,7 @@
 // TODO: restrict `renderer` input?
 export interface Basis {
+  /** indicate what kind of visualization this spec is about */
+  type?: 'chart' | 'graph';
   width?: number;
   height?: number;
   padding?: number[] | number | 'auto';
