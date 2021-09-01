20ca6ec46e75880b121da3c2dfabaaa51e54c81e
soundquiet <625941917@qq.com>
2021-09-01 18:08:21 +0800

    feat: scale property of color enc

--- src/schema/encoding/color.ts
+++ src/schema/encoding/color.ts
@@ -1,8 +1,10 @@
 import { EType } from './type';
 import { Aggregate } from './aggregate';
+import { Scale } from './scale';
 
 export interface Color {
   field: string;
   type: EType;
   aggregate?: Aggregate;
+  scale?: Scale;
 }
