7a599f3d40aaa19d747087ef7782a9b5a63c6c09
soundquiet <625941917@qq.com>
2021-09-02 10:59:02 +0800

    fix: set scale property optional

--- src/schema/encoding/scale.ts
+++ src/schema/encoding/scale.ts
@@ -2,12 +2,12 @@
 export type Scale = {
   // range after data mapping
   // such as ['apple', 'banana'] --map to--> ['red', 'yellow'](range)
-  range: (string | number)[];
-  rangeMin: string | number;
-  rangeMax: string | number;
+  range?: (string | number)[];
+  rangeMin?: string | number;
+  rangeMax?: string | number;
   // data domain used to data mapping
   // such as [0, 100] --domain:[10, 90]--> [10, 90](filter data less than 10)
-  domain: number[];
-  domainMin: number;
-  domaminMax: number;
+  domain?: number[];
+  domainMin?: number;
+  domaminMax?: number;
 };
